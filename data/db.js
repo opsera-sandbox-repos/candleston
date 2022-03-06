9d1ccd7c1bb1b2747b3ccb886bdc84a4b865c02a
apat35 <sparuna@protonmail.com>
2022-03-06 21:21:47 +0400

    db configurations

---
+++ data/db.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+mongoose.connect(dburl);
+
+mongoose.connection.on('connected', function(){
+    console.log("Mongoose Connected to " + dburl);
+});
+mongoose.connection.on('disconnected', function(){
+    console.log("Mongoose Disconnected");
+});
+mongoose.connection.on('error', function(err){
+    console.log("Mongoose Connection Error: " + err);
+});
+
+process.on('SIGINT', function(){
+    mongoose.connection.close(function(){
+        console.log("Mongoose disconnected through app termination");
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function(){
+    mongoose.connection.close(function(){
+        console.log('Mongoose disconnected through app termination (SIGTERM)');
+        process.exit(0);
+    });
+});
+
+
+process.once('SIGUSR2', function(){
+    mongoose.connection.close(function(){
+        console.log('Mongoose disconnected through app termination (SIGUSR2)');
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+require('./users.model.js');
