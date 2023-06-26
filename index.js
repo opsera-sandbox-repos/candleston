0e8dd92271544023117199d61b6cbfa12f8aec23
apat35 <93975209+apat35@users.noreply.github.com>
2023-06-26 22:53:40 +0400

    DT-155 Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("testing candleston app success.. will run on port 3000");
 
 console.log("error in stage... rollback to dev");
+
+console.log("testing prod hooks to generate more data");
